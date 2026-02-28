85ab69f53e77c467d3373823cda290fd3a912cb7
Arda <85059535+ardatas@users.noreply.github.com>
2026-02-28 20:06:49 +0300

    inital commit

---
+++ src/test/java/com/ardatas/schedal/SchedalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardatas.schedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
